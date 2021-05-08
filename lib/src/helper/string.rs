97256415e16b2ec0c39afa044971f5b9dd2bd31a
Paul Pham <paul@heratap.com>
2021-05-08 16:55:38 +0700

    Add UTF16 util

--- lib/src/helper/string.rs
+++ lib/src/helper/string.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::iter::once;
 
 pub fn remove_whitespace<'a>(input: &'a str) -> Cow<'a, str> {
     if input.contains(' ') {
@@ -9,4 +10,30 @@ pub fn remove_whitespace<'a>(input: &'a str) -> Cow<'a, str> {
         .into();
     }
     return input.into();
+}
+
+#[cfg(windows)]
+pub fn encode_wide(s: &str) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+
+    OsStr::new(s).encode_wide().collect()
+}
+
+#[cfg(not(windows))]
+pub fn encode_wide(s: &str) -> Vec<u16> {
+    s.encode_utf16().collect()
+}
+
+#[cfg(windows)]
+pub fn encode_wide_c(s: &str) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+
+    OsStr::new(s).encode_wide().chain(once(0)).collect()
+}
+
+#[cfg(not(windows))]
+pub fn encode_wide_c(s: &str) -> Vec<u16> {
+    s.encode_utf16().chain(once(0)).collect()
 }

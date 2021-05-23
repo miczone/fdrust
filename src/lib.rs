637fc6de94f1b09d7aea44f3c32ca49c4e236536
Paul Pham <paul@heratap.com>
2021-05-23 12:22:08 +0700

    Add interval tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![recursion_limit = "400"]
+
+#[macro_use]
+extern crate serde_derive;
+
+#[cfg(feature = "std")]
+extern crate std;
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
 pub mod array;
 pub mod wasm;
 pub mod helper;
